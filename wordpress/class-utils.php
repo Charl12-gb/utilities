f7da39afb9536323450ae93f3627bcd78df8110d
GBOYOU Charles <gboyoucharles22@gmail.com>
2022-12-25 13:31:40 +0100

    Test & Correction

--- wordpress/class-utils.php
+++ wordpress/class-utils.php
@@ -11,6 +11,7 @@ if (!class_exists('Utilities')) {
 
         public static function admin_form($data)
         {
+			global $o_row_templates;
             ob_start();
             foreach ($data as $champ) {
                 if (!isset($champ['type']))
@@ -29,20 +30,21 @@ if (!class_exists('Utilities')) {
                     $champ['title'] = isset($champ['name']) ? $champ['name'] : '';
                 if (!isset($champ['description']))
                     $champ['description'] = '';
+                if (!isset($champ['options']))
+                    $champ['options'] = array();
                 if (!isset($champ['placeholder']))
                     $champ['placeholder'] = '';
+                if (!isset($champ['btn_value']))
+                    $champ['btn_value'] = '';
+				if ( ! isset( $champ['row_class'] ) ) 
+					$champ['row_class'] = '';
+				if ( ! isset( $champ['row_css'] ) )
+					$champ['row_css'] = '';
+				if ( ! isset( $champ['ig_desc'] ) ) 
+					$champ['ig_desc'] = false;
 
-                // Custom attribute handling.
-                $custom_attributes = array();
 
-                if (!empty($champ['custom_attributes']) && is_array($champ['custom_attributes'])) {
-                    foreach ($champ['custom_attributes'] as $attribute => $attribute_champ) {
-                        $custom_attributes[] = esc_attr($attribute) . '="' . esc_attr($attribute_champ) . '"';
-                    }
-                }
-
-
-                $description = $champ['desc'];
+                $description = $champ['description'];
 
                 if ($description && in_array($champ['type'], array('textarea', 'radio'))) {
                     $description = "<p style='margin-top:0'> $description </p>";
@@ -54,8 +56,8 @@ if (!class_exists('Utilities')) {
                 $champ_value    = '';
                 $raw_hierarchy   = self::explodes_x(array('[', ']'), $champ['name']);
                 $hierarchy       = array_filter($raw_hierarchy);
-                $section_types   = array('sectionbegin', 'sectionend');
-                $settings_table  = self::get_right_value($champ[0], 'table', 'metas');
+                $section_types   = array('sectionstart', 'sectionend');
+                $settings_table  = self::get_right_value($data[0], 'table', 'metas');
 
                 if (!in_array($champ['type'], $section_types) & !empty($hierarchy)) {
                     $root_key    = $hierarchy[0];
@@ -67,10 +69,11 @@ if (!class_exists('Utilities')) {
                         $champ_value       = self::get_right_value($_SESSION['util-data'], $session_key, false);
                     }
 
+					
                     if (!$champ_value) {
                         // Retrieve from the metas.
                         if ('metas' == $settings_table) {
-                            $champ_value                       = get_post_meta($post_id, $root_key, true);
+							$champ_value                       = get_post_meta($post_id, $root_key, true);
                             $_SESSION['util-data'][$session_key] = $champ_value;
                         } elseif ('options' == $settings_table) { // Retrieve from the options.
                             $champ_value                       = get_option($root_key);
@@ -80,31 +83,35 @@ if (!class_exists('Utilities')) {
                             $_SESSION['util-data'][$session_key] = $champ_value;
                         }
                     }
-
+					
                     $session_key = $root_key . "_$post_id";
                     $root_value  = self::get_right_value($_SESSION['util-data'], $session_key, false);
                     if ($root_key != $champ['name']) {
-                        $champ_value = self::find_in_array_by_key($root_value, $champ['name']);
+						$champ_value = self::find_in_array_by_key($root_value, $champ['name']);
                     }
                 }
+				if ( ! $champ_value && '0' !== $champ_value ) {
+					$champ_value = $champ['default'];
+				}
 
-                if (!in_array($champ['type'], $section_types)) {
-                    $description = $champ['description'];
-                    ?>
-                    <td class='label'>
-                        <?php
-                        echo wp_kses_post($champ['title']);
-                        echo wp_kses_post("<div class=''>" . $description . '</div>');
-                        ?>
-                    </td>
-                    <?php
-                }
+				if ( ! in_array( $champ['type'], $section_types ) && ! $champ['ig_desc'] ) {
+					$descrip = $champ['description'];
+					?>
+				<tr style="<?php echo esc_attr( $champ['row_css'] ); ?>" class="<?php echo esc_attr( $champ['row_class'] ); ?>">
+				<td class=''>
+					<?php
+					echo wp_kses_post( $champ['title'] );
+					echo wp_kses_post( "<div class=''>" . $descrip . '</div>' );
+					?>
+				</td>
+					<?php
+				}
 
                 switch ($champ['type']) {
                     case 'sectionstart':
                         // We start/reset the session.
                         $_SESSION['util-data'] = array();
-                        section_start();
+                        section_start($champ['id']);
                         break;
                     case 'sectionend':
                         section_end();
@@ -114,6 +121,8 @@ if (!class_exists('Utilities')) {
                     case 'number':
                     case 'password':
                     case 'color':
+                    case 'date':
+                    case 'hour':
                         $type = $champ['type'];
                         input_x($champ['name'], $champ['id'], $type, $champ['css'], $champ_value, $champ['class'], $champ['placeholder']);
                         break;
@@ -125,7 +134,7 @@ if (!class_exists('Utilities')) {
                         break;
                     case 'select':
                     case 'multiselect':
-                        select_x($champ['name'], $champ['id'], $champ['type'], $champ['css'], $champ['class']);
+                        select_x($champ['name'], $champ['id'], $champ['type'], $champ['css'], $champ['class'], $champ['options'], $champ_value);
                         break;
                     case 'radio':
                         radio_x($champ['options'], $champ['name'], $champ['css'], $champ['class'], $champ_value);
@@ -136,6 +145,12 @@ if (!class_exists('Utilities')) {
                     case 'file':
                         file_x($champ['name'], $champ['class'], $champ_value);
                         break;
+					case 'submit':
+						button_submit_x($champ['id'], $champ['css'], $champ['btn_value'], $champ['class'], $champ['title']);
+						break;
+					default:
+						do_action( 'admin_form_' . $champ['type'], $champ );
+						break;
                 }
             }
             return ob_get_clean();
@@ -191,5 +206,260 @@ if (!class_exists('Utilities')) {
             }
             return $root_value;
         }
+
+        /**
+		 * Function to load allowed html tags.
+		 *
+		 * @return array
+		 */
+		public static function get_allowed_balises() {
+			$allowed_balises = wp_kses_allowed_html( 'post' );
+			add_filter(
+				'safe_style_css',
+				function( $styles ) {
+					$styles[] = 'display';
+					return $styles;
+				}
+			);
+
+			$allowed_balises['li'] = array(
+				'id'             => array(),
+				'name'           => array(),
+				'class'          => array(),
+				'value'          => array(),
+				'style'          => array(),
+				'data-ttf'       => array(),
+				'data-fonturl'   => array(),
+				'data-fontname'  => array(),
+				'data-color'     => array(),
+				'data-minwidth'  => array(),
+				'data-minheight' => array(),
+			);
+
+			$allowed_balises['br'] = array();
+
+			$allowed_balises['input'] = array(
+				'type'           => array(),
+				'id'             => array(),
+				'name'           => array(),
+				'style'          => array(),
+				'class'          => array(),
+				'value'          => array(),
+				'min'            => array(),
+				'max'            => array(),
+				'row_class'      => array(),
+				'selected'       => array(),
+				'checked'        => array(),
+				'readonly'       => array(),
+				'placeholder'    => array(),
+				'step'           => array(),
+				'data-fonturl'   => array(),
+				'data-fontname'  => array(),
+				'data-minwidth'  => array(),
+				'data-minheight' => array(),
+				'autocomplete'   => array(),
+				'autocorrect'    => array(),
+				'autocapitalize' => array(),
+				'spellcheck'     => array(),
+				'pattern'        => array(),
+				'required'       => array(),
+			);
+			$allowed_balises['form']  = array(
+				'accept-charset' => array(),
+				'id'             => array(),
+				'name'           => array(),
+				'style'          => array(),
+				'class'          => array(),
+				'value'          => array(),
+				'action'         => array(),
+				'autocomplete'   => array(),
+				'row_class'      => array(),
+				'novalidate'     => array(),
+				'method'         => array(),
+				'readonly'       => array(),
+				'target'         => array(),
+				'data-fonturl'   => array(),
+				'data-fontname'  => array(),
+				'data-minwidth'  => array(),
+				'data-minheight' => array(),
+				'autocorrect'    => array(),
+				'autocapitalize' => array(),
+				'hidden'         => array(),
+			);
+
+			$allowed_balises['div'] = array(
+				'id'                   => array(),
+				'name'                 => array(),
+				'data-id'              => array(),
+				'class'                => array(),
+				'row_class'            => array(),
+				'role'                 => array(),
+				'aria-labelledby'      => array(),
+				'aria-hidden'          => array(),
+				'data-fonturl'         => array(),
+				'data-minwidth'        => array(),
+				'data-minheight'       => array(),
+				'data-tooltip-content' => array(),
+				'tabindex'             => array(),
+				'style'                => array(),
+				'data-tooltip-title'   => array(),
+				'data-placement'       => array(),
+				'media'                => array(),
+			);
+			$allowed_balises['i']   = array();
+
+			$allowed_balises['button'] = array(
+				'id'                => array(),
+				'name'              => array(),
+				'class'             => array(),
+				'value'             => array(),
+				'data-tpl'          => array(),
+				'style'             => array(),
+				'data-id'           => array(),
+				'data-dismiss'      => array(),
+				'aria-hidden'       => array(),
+				'data-editor'       => array(),
+				'type'              => array(),
+				'data-wp-editor-id' => array(),
+			);
+
+			$allowed_balises['body'] = array(
+				'id'                 => array(),
+				'name'               => array(),
+				'class'              => array(),
+				'data-gr-c-s-loaded' => array(),
+			);
+
+			$allowed_balises['a']        = array(
+				'id'               => array(),
+				'name'             => array(),
+				'class'            => array(),
+				'data-tpl'         => array(),
+				'href'             => array(),
+				'data-toggle'      => array(),
+				'data-target'      => array(),
+				'data-modalid'     => array(),
+				'target'           => array(),
+				'data-group'       => array(),
+				'data-slide-index' => array(),
+				'download'         => array(),
+				'style'            => array(),
+			);
+			$allowed_balises['select']   = array(
+				'id'         => array(),
+				'name'       => array(),
+				'class'      => array(),
+				'data-tpl'   => array(),
+				'style'      => array(),
+				'multiple'   => array(),
+				'tabindex'   => array(),
+				'data-rule'  => array(),
+				'data-group' => array(),
+			);
+			$allowed_balises['optgroup'] = array(
+				'id'       => array(),
+				'name'     => array(),
+				'class'    => array(),
+				'data-tpl' => array(),
+				'style'    => array(),
+				'multiple' => array(),
+				'tabindex' => array(),
+				'label'    => array(),
+			);
+			$allowed_balises['option']   = array(
+				'id'       => array(),
+				'name'     => array(),
+				'class'    => array(),
+				'value'    => array(),
+				'style'    => array(),
+				'selected' => array(),
+				'tabindex' => array(),
+			);
+
+			$allowed_balises['span'] = array(
+				'id'                 => array(),
+				'name'               => array(),
+				'class'              => array(),
+				'value'              => array(),
+				'style'              => array(),
+				'data-tooltip-title' => array(),
+				'data-placement'     => array(),
+			);
+
+			$allowed_balises['h1']     = array(
+				'id'    => array(),
+				'class' => array(),
+				'style' => array(),
+			);
+			$allowed_balises['iframe'] = array();
+			$allowed_balises['h2']     = array(
+				'id'    => array(),
+				'class' => array(),
+				'style' => array(),
+			);
+			$allowed_balises['h3']     = array(
+				'style' => array(),
+				'id'    => array(),
+				'class' => array(),
+			);
+
+			$allowed_balises['link'] = array(
+				'id'    => array(),
+				'rel'   => array(),
+				'media' => array(),
+				'href'  => array(),
+			);
+
+			$allowed_balises['textarea'] = array(
+				'autocomplete'   => array(),
+				'autocorrect'    => array(),
+				'autocapitalize' => array(),
+				'spellcheck'     => array(),
+				'class'          => array(),
+				'rows'           => array(),
+				'cols'           => array(),
+				'name'           => array(),
+				'id'             => array(),
+				'style'          => array(),
+			);
+
+			$allowed_balises['table'] = array(
+				'border'      => array(),
+				'cellpadding' => array(),
+				'cellspacing' => array(),
+				'class'       => array(),
+				'style'       => array(),
+			);
+
+			$allowed_balises['tr'] = array(
+				'align'   => array(),
+				'class'   => array(),
+				'style'   => array(),
+				'data-id' => array(),
+			);
+
+			$allowed_balises['td'] = array(
+				'colspan' => array(),
+				'class'   => array(),
+				'style'   => array(),
+			);
+
+			$allowed_balises['th'] = array(
+				'colspan' => array(),
+				'class'   => array(),
+				'style'   => array(),
+			);
+
+			$allowed_balises['img'] = array(
+				'src'    => array(),
+				'alt'    => array(),
+				'height' => array(),
+				'width'  => array(),
+				'style'  => array(),
+				'class'  => array(),
+			);
+
+			return apply_filters( 'allowed_balises', $allowed_balises );
+		}
     }
 }
--- wordpress/composant.php
+++ wordpress/composant.php
@@ -1,11 +1,10 @@
 <?php 
 
-function section_start(){
+function section_start($id){
     ?>
     <div class="wrap">
-    <div id="<?php echo esc_attr($champ['id']); ?>" class="metabox-container">
-    <div class='block-form'>
-    <table class="wp-list-table widefat fixed pages">
+    <div id="<?php echo esc_attr($id); ?>" class="metabox-container">
+    <table class="">
     <tbody></tbody>
     <?php
 }
@@ -16,38 +15,42 @@ function section_end(){
     </table>
     </div>
     </div>
-    </div>
     <?php
 }
 
 // Standard text inputs and subtypes like 'number'
 function input_x($name, $id, $type, $style, $value, $class, $placeholder){
     ?>
-    <input
-		name="<?php echo esc_attr( $name ); ?>"
-		id="<?php echo esc_attr( $id ); ?>"
-		type="<?php echo esc_attr( $type ); ?>"
-		style="<?php echo esc_attr( $style ); ?>"
-		value="<?php echo esc_attr( $value ); ?>"
-		class="<?php echo esc_attr( $class ); ?>"
-		placeholder="<?php echo esc_attr( $placeholder ); ?>"/>
+    <td>
+        <input
+            name="<?php echo esc_attr( $name ); ?>"
+            id="<?php echo esc_attr( $id ); ?>"
+            type="<?php echo esc_attr( $type ); ?>"
+            style="<?php echo esc_attr( $style ); ?>"
+            value="<?php echo esc_attr( $value ); ?>"
+            class="<?php echo esc_attr( $class ); ?>"
+            placeholder="<?php echo esc_attr( $placeholder ); ?>"/>
+        </td>
     <?php
 }
 
 function textarea_x($name, $id, $style, $value, $class, $placeholder){
     ?>
-    <textarea 
-        name="<?php echo esc_attr( $name ); ?>"
-		id="<?php echo esc_attr( $id ); ?>"
-		style="<?php echo esc_attr( $style ); ?>"
-        placeholder="<?php echo esc_attr( $placeholder ); ?>"
-		class="<?php echo esc_attr( $class ); ?>">
-		<?php echo esc_textarea( $value ); ?>
-	</textarea>
+    <td>
+        <textarea 
+            name="<?php echo esc_attr( $name ); ?>"
+            id="<?php echo esc_attr( $id ); ?>"
+            style="<?php echo esc_attr( $style ); ?>"
+            placeholder="<?php echo esc_attr( $placeholder ); ?>"
+            class="<?php echo esc_attr( $class ); ?>">
+            <?php echo esc_textarea( $value ); ?>
+        </textarea>
+    <td>
     <?php
 }
 
 function texteditor_x($name, $id, $value){
+    ?><td><?php
     wp_editor(
         $value,
         $id,
@@ -58,43 +61,46 @@ function texteditor_x($name, $id, $value){
             'textarea_rows' => 10,
         )
     );
+    ?></td><?php
 }
 
-function select_x($name, $id, $type, $style, $class){
+function select_x($name, $id, $type, $style, $class, $options, $value){
 
 	$select_is_multiple = '';
 	if ( 'multiselect' === $type ) {
 		$select_is_multiple = ' multiple="multiple"';
 	}
 	?>
-    <select 
-        name="<?php echo esc_attr( $name ); ?>"
-		id="<?php echo esc_attr( $id ); ?>"
-		style="<?php echo esc_attr( $style ); ?>"
-		class="<?php echo esc_attr( $class ); ?>"
-		<?php echo esc_attr( $select_is_multiple ); ?> >
-		<?php
-		foreach ( $value['options'] as $key => $val ) {
-			?>
-			<option value="<?php echo esc_attr( $key ); ?>"
-			<?php 
-			if ( is_array( $value ) ) {
-				selected( in_array( $key, $value, false ), true );
-			} else {
-				selected( $value, $key );
-			}
-			?>>
-            <?php echo esc_html( $val ); ?></option>
-			<?php
-		}
-		?>
-	</select>
+    <td>
+        <select 
+            name="<?php echo esc_attr( $name ); ?>"
+            id="<?php echo esc_attr( $id ); ?>"
+            style="<?php echo esc_attr( $style ); ?>"
+            class="<?php echo esc_attr( $class ); ?>"
+            <?php echo esc_attr( $select_is_multiple ); ?> >
+            <?php
+            foreach ( $options as $key => $val ) {
+                ?>
+                <option value="<?php echo esc_attr( $key ); ?>"
+                <?php 
+                if ( is_array( $value ) ) {
+                    selected( in_array( $key, $value, false ), true );
+                } else {
+                    selected( $value, $key );
+                }
+                ?>>
+                <?php echo esc_html( $val ); ?></option>
+                <?php
+            }
+            ?>
+        </select>
+    </td>
     <?php
 }
 
 function checkbox_x($name, $title, $value, $id, $style, $class, $description){
     if ( $title != '' ) {
-		?>
+		?><td>
 		<legend>
             <span><?php echo esc_html( $title ); ?></span>
 		</legend>
@@ -113,11 +119,13 @@ function checkbox_x($name, $title, $value, $id, $style, $class, $description){
             <?php checked( $value, $value ); ?> />
         <?php echo wp_kses_post( $description ); ?>
 	</label>
+    </td>
 	<?php					
 }
 
 function radio_x(array $options, $name, $css, $class, $value){
     ?>
+    <td>
 	<fieldset>
 		<ul>
             <?php
@@ -139,11 +147,13 @@ function radio_x(array $options, $name, $css, $class, $value){
             ?>
 		</ul>
 	</fieldset>
+    </td>
 	<?php
 }
 
 function file_x($name, $class, $value){
     ?>
+    <td>
     <div class="<?php echo esc_attr( $class ); ?>">
         <input type="hidden" name="<?php echo esc_attr( $name ); ?>"
             value="<?php echo esc_attr( $value ); ?>">
@@ -155,5 +165,20 @@ function file_x($name, $class, $value){
             ?>
         </div>
     </div>
+    </td>
     <?php
+}
+
+function button_submit_x($id, $style, $value, $class, $title){
+    ?>
+    <td>
+        <button 
+            id="<?php echo esc_attr( $id ); ?>" 
+            style="<?php echo esc_attr( $style ); ?>"
+            value="<?php echo esc_attr( $value ); ?>"
+            class="<?php echo esc_attr( $class ); ?>" 
+            type="submit">
+            <?php echo esc_attr( $title ); ?></button>
+    </td>
+	<?php
 }
